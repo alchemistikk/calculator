92c17ff3e99fc1ee098b00fa3f1ca1218ea91388
Mike Mitchell <alchemistikk@gmail.com>
2021-10-10 07:18:49 -0400

    Add function to clear all values and display

--- script.js
+++ script.js
@@ -16,40 +16,65 @@ function operate(operator, x, y) {
   }
 }
 
-let displayValue = 0;
+let displayValue = '';
 let firstOperand;
 let operator;
 let secondOperand;
+let evaluated = false;
 
 const displayNode = document.querySelector("#display");
 const allNumbers = document.querySelectorAll(".operand");
 const allOperators = document.querySelectorAll(".operator");
 const equalsOperator = document.querySelector("#equals");
+const clearOperator = document.querySelector("#clear");
 
 allNumbers.forEach(number => {
   number.addEventListener('click', () => {
-    displayNode.textContent += (number.textContent);
-    displayValue += (number.textContent); 
+    if (evaluated === true) {
+      displayValue = '';
+      displayNode.textContent = '';
+      displayValue += (number.textContent); 
+      displayNode.textContent += (number.textContent);
+      evaluated = false;
+    } else {
+      displayValue += (number.textContent); 
+      displayNode.textContent += (number.textContent);
+    }
   })
-})
+});
 
 allOperators.forEach(operation => {
   operation.addEventListener('click', () => {
     firstOperand = Number(displayValue);
     operator = operation.id;
-    displayValue = 0;
-    displayNode.textContent = null;
+    displayValue = '';
+    displayNode.textContent = '';
   })
-})
+});
 
-equalsOperator.addEventListener('click', () => {
+equalsOperator.addEventListener('click', () => evaluate());
+clearOperator.addEventListener('click', () => {
+  clearValues();
+  clearDisplay();
+});
+
+function evaluate () {
   secondOperand = Number(displayValue);
+  displayValue = operate(operator, firstOperand, secondOperand);
   displayNode.textContent = operate(operator, firstOperand, secondOperand);
-  console.log(firstOperand);
-  console.log(secondOperand);
+  evaluated = true;
+  clearValues();
+}
+
+function clearValues () {
   firstOperand = undefined;
   operator = undefined;
   secondOperand = undefined;
-})
+  displayValue = '';
+}
+
+function clearDisplay () {
+  displayNode.textContent = '';
+}
 
 // If equals pressed, then operand pressed, set display to operand
