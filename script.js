0f2dd0cd22bb3a11bf82a52469fd05ae5fd880a5
Mike Mitchell <alchemistikk@gmail.com>
2021-10-09 22:23:15 -0400

    Add button and display implementations of operations

--- script.js
+++ script.js
@@ -2,15 +2,54 @@ let add = (x, y) => x + y;
 let subtract = (x, y) => x - y;
 let multiply = (x, y) => x * y;
 let divide = (x, y) => x / y;
-let operate = (operator, x, y) => operator(x, y);
+
+function operate(operator, x, y) {
+  switch (operator) {
+      case "add":
+          return add (x, y);
+      case "subtract":
+          return subtract (x, y);
+      case "multiply":
+          return multiply (x, y);
+      case "divide":
+          return divide (x, y); 
+  }
+}
 
 let displayValue = 0;
+let firstOperand;
+let operator;
+let secondOperand;
+
 const displayNode = document.querySelector("#display");
 const allNumbers = document.querySelectorAll(".operand");
+const allOperators = document.querySelectorAll(".operator");
+const equalsOperator = document.querySelector("#equals");
 
 allNumbers.forEach(number => {
   number.addEventListener('click', () => {
-    displayNode.textContent = number.textContent;
-    displayValue = number.textContent; 
+    displayNode.textContent += (number.textContent);
+    displayValue += (number.textContent); 
   })
 })
+
+allOperators.forEach(operation => {
+  operation.addEventListener('click', () => {
+    firstOperand = Number(displayValue);
+    operator = operation.id;
+    displayValue = 0;
+    displayNode.textContent = null;
+  })
+})
+
+equalsOperator.addEventListener('click', () => {
+  secondOperand = Number(displayValue);
+  displayNode.textContent = operate(operator, firstOperand, secondOperand);
+  console.log(firstOperand);
+  console.log(secondOperand);
+  firstOperand = undefined;
+  operator = undefined;
+  secondOperand = undefined;
+})
+
+// If equals pressed, then operand pressed, set display to operand
